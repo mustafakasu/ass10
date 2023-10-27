441426d060ae897e53f0dfda42c9c9d16e945a72
mustafakasu <nustafakau4@gmail.com>
2023-10-27 07:48:06 -0700

    practice

---
+++ q3.py
@@ -0,0 +1,13 @@
+def prit(n, isPrime):
+    isPrime[0] = isPrime[1] = False
+    for i in range(2,n):
+        isPrime[i] = True
+        for p in range(2,n+1):
+            if (p*p<=n and isPrime[p] == True):
+                for i in range(p*2,n+1,p):
+                    isPrime[i] = False
+                    p += 1
+def superPrimes(n):
+    isPrime = [1 for i in range(n+1)]
+    prit(n, isPrime)
+    
