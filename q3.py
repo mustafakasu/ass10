15b227dd2b82547ba91d7160321c9e4b0a413083
mustafakasu <nustafakau4@gmail.com>
2023-10-27 07:49:51 -0700

    ndn

--- q3.py
+++ q3.py
@@ -1,7 +1,7 @@
 def prit(n, isPrime):
     isPrime[0] = isPrime[1] = False
     for i in range(2,n):
-        isPrime[i] = True
+        aisPrime[i] = True
         for p in range(2,n+1):
             if (p*p<=n and isPrime[p] == True):
                 for i in range(p*2,n+1,p):
